6c31d7ce9c8ef22a13b093215c01753002d03656
Renuka Mate <renuka.mate@expleogroup.com>
2023-04-20 10:58:17 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from vercel use flask"
+
+@app.route("/about")
+def about():
+    return "Hello about"
