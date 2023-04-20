c943c7e2682da510f1bc45c293a56a72e7782243
Renuka Mate <renuka.mate@expleogroup.com>
2023-04-20 11:49:48 +0530

    added upload file function

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import *
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello from vercel use flask"
+def index():
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "Hello about"
+@app.route('/', methods = ['POST'])
+def uploadFile():
+    global text_col
+    global df
+    global selected_columns
+    file = request.files['file']
+    if file.filename.endswith('.csv'):
+        df = pd.read_csv(file)
+    elif file.filename.endswith('.xlsx'):
+        df = pd.read_excel(file)
+    else:
+        return "Invalid file format. Please upload a CSV or Excel file."
+    selected_columns = []
+    text_col = []
+    for i in df.columns:
+        if df[i].dtype == 'object':
+            text_col.append(i)
+    if len(text_col)>1:
+        return render_template('page1.html', text_col=text_col)    
+
+    return render_template('page1.html')
 
 if __name__ == '__main__':  
     app.run(debug=True)
