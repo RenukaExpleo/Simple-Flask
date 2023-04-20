a86b627cde4fd2af36b9d0f3ca9325cda07fddcf
Renuka Mate <renuka.mate@expleogroup.com>
2023-04-20 15:03:23 +0530

    added code

--- main.py
+++ main.py
@@ -1,46 +1,46 @@
-from flask import *
 import pandas as pd
+from flask import *
+import os
 
 app = Flask(__name__)
 
 global df
-global text_col
 global selected_columns
+global text_col
 
-@app.route("/")
+@app.route('/') 
 def index():
     return render_template("index.html")
 
 @app.route('/', methods = ['POST'])
 def uploadFile():
     global text_col
-    global df
-    global selected_columns
     file = request.files['file']
     if file.filename.endswith('.csv'):
-        df = pd.read_csv(file)
+        file_path = os.path.join('files/', file.filename)
+        file.save(file_path)
+        df = pd.read_csv(file_path)
     elif file.filename.endswith('.xlsx'):
         df = pd.read_excel(file)
     else:
         return "Invalid file format. Please upload a CSV or Excel file."
-    selected_columns = []
     text_col = []
     for i in df.columns:
         if df[i].dtype == 'object':
             text_col.append(i)
     if len(text_col)>1:
-        return render_template('page1.html', text_col=text_col)    
-
+        return render_template('page1.html', text_col=text_col) 
     return render_template('page1.html')
+
 
 @app.route('/preview')
 def previewData():
-    global df
-    data_head =df.head().to_html(justify='left')
-    n_rows = df.shape[0]
-    n_cols = df.shape[1]
-    return render_template('page1.html', data_head = data_head, n_rows=n_rows, n_cols=n_cols)
-    # return "preview data"
+# global text_col
+   df = pd.read_csv('files/'+'HousingData.csv')
+   data_head =df.head().to_html()
+   n_rows = df.shape[0]
+   n_cols = df.shape[1]
+   return render_template('page1.html', data_head = data_head, n_rows=n_rows, n_cols=n_cols, text_col=text_col)
 
 if __name__ == '__main__':  
     app.run(debug=True)
